4fb02cc265904df1258b121127a678f20968fcae
Josh Nichols <joshua.nichols@gmail.com>
2022-06-27 22:43:13 -0400

    Add icons back after fixing icons

--- lua/ui.lua
+++ lua/ui.lua
@@ -85,10 +85,10 @@ vim.o.showmode = false
 local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
 dashboard.section.buttons.val = {
-	dashboard.button("e", "New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("r", "Restore session", "<Cmd>RestoreSession<CR>"),
-	dashboard.button("p", "Open Project", "<Cmd>Telescope projects<CR>"),
-	dashboard.button("q", "Quit NVIM", ":qa<CR>"),
+	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("r", "  Restore session", "<Cmd>RestoreSession<CR>"),
+	dashboard.button("p", "  Open Project", "<Cmd>Telescope projects<CR>"),
+	dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
 }
 alpha.setup(dashboard.config)
 
