5e078a4f7b3fcaf4ffd88eb3e164f13e05e1f004
Josh Nichols <joshua.nichols@gmail.com>
2022-07-04 12:09:16 -0400

    Add and configure wilder

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,170 +6,151 @@ vim.cmd([[
 ]])
 
 return require("packer").startup(function(use)
-	-- package manager
-	use("wbthomason/packer.nvim")
+    -- package manager
+    use("wbthomason/packer.nvim")
 
-	-- lsp, linters, formatters, etc
-	use({
-		"VonHeikemen/lsp-zero.nvim",
-		requires = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" },
-			{ "williamboman/nvim-lsp-installer" },
+    -- lsp, linters, formatters, etc
+    use({
+        "VonHeikemen/lsp-zero.nvim",
+        requires = {
+            -- LSP Support
+            {"neovim/nvim-lspconfig"}, {"williamboman/nvim-lsp-installer"},
 
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" },
-			{ "hrsh7th/cmp-buffer" },
-			{ "hrsh7th/cmp-path" },
-			{ "saadparwaiz1/cmp_luasnip" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "hrsh7th/cmp-nvim-lua" },
-			{ "hrsh7th/cmp-nvim-lsp-signature-help" },
-			{ "mtoohey31/cmp-fish" },
+            -- Autocompletion
+            {"hrsh7th/nvim-cmp"}, {"hrsh7th/cmp-buffer"}, {"hrsh7th/cmp-path"},
+            {"saadparwaiz1/cmp_luasnip"}, {"hrsh7th/cmp-nvim-lsp"},
+            {"hrsh7th/cmp-nvim-lua"}, {"hrsh7th/cmp-nvim-lsp-signature-help"},
+            {"mtoohey31/cmp-fish"}, -- Snippets
+            {"L3MON4D3/LuaSnip"}, {"rafamadriz/friendly-snippets"}
+        }
+    })
+    use("jose-elias-alvarez/null-ls.nvim")
+    use("onsails/lspkind.nvim")
 
-			-- Snippets
-			{ "L3MON4D3/LuaSnip" },
-			{ "rafamadriz/friendly-snippets" },
-		},
-	})
-	use("jose-elias-alvarez/null-ls.nvim")
-	use("onsails/lspkind.nvim")
+    -- Speed up loading Lua modules in Neovim to improve startup time.
+    -- Load before ay other lua plugins
+    use({
+        "lewis6991/impatient.nvim",
+        config = function() require("impatient") end
+    })
 
-	-- Speed up loading Lua modules in Neovim to improve startup time.
-	-- Load before ay other lua plugins
-	use({
-		"lewis6991/impatient.nvim",
-		config = function()
-			require("impatient")
-		end,
-	})
+    -- drop in replacement for filetype.vim
+    use("nathom/filetype.nvim")
 
-	-- drop in replacement for filetype.vim
-	use("nathom/filetype.nvim")
+    use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
 
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-	})
+    use("nvim-treesitter/nvim-treesitter-refactor")
+    use("RRethy/nvim-treesitter-endwise")
 
-	use("nvim-treesitter/nvim-treesitter-refactor")
-	use("RRethy/nvim-treesitter-endwise")
+    -- styling cursor, ident lines, etc
+    use("yamatsum/nvim-cursorline")
+    use("lukas-reineke/indent-blankline.nvim")
+    use("p00f/nvim-ts-rainbow")
 
-	-- styling cursor, ident lines, etc
-	use("yamatsum/nvim-cursorline")
-	use("lukas-reineke/indent-blankline.nvim")
-	use("p00f/nvim-ts-rainbow")
+    use("folke/which-key.nvim")
 
-	use("folke/which-key.nvim")
+    -- dashboard when starting à la startify
+    use({"goolord/alpha-nvim", requires = {"kyazdani42/nvim-web-devicons"}})
 
-	-- dashboard when starting à la startify
-	use({
-		"goolord/alpha-nvim",
-		requires = { "kyazdani42/nvim-web-devicons" },
-	})
+    use("tpope/vim-commentary")
 
-	use("tpope/vim-commentary")
+    use("JoosepAlviste/nvim-ts-context-commentstring")
 
-	use("JoosepAlviste/nvim-ts-context-commentstring")
+    -- customizable statusline with nice defaults
+    use({
+        "nvim-lualine/lualine.nvim",
+        requires = {"kyazdani42/nvim-web-devicons", opt = true}
+    })
 
-	-- customizable statusline with nice defaults
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-	})
+    -- Find, Filter, Preview, Pick. All lua, all the time.
+    use({
+        "nvim-telescope/telescope.nvim",
+        requires = {{"nvim-lua/plenary.nvim"}}
+    })
 
-	-- Find, Filter, Preview, Pick. All lua, all the time.
-	use({
-		"nvim-telescope/telescope.nvim",
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
+    use({"nvim-telescope/telescope-fzf-native.nvim", run = "make"})
 
-	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+    use({
+        "gfeiyou/command-center.nvim",
+        requires = {"nvim-telescope/telescope.nvim"}
+    })
 
-	use({
-		"gfeiyou/command-center.nvim",
-		requires = { "nvim-telescope/telescope.nvim" },
-	})
+    -- terminal manager
+    use({"akinsho/toggleterm.nvim", tag = "v1.*"})
 
-	-- terminal manager
-	use({
-		"akinsho/toggleterm.nvim",
-		tag = "v1.*",
-	})
+    -- tree explorer
+    use({
+        "kyazdani42/nvim-tree.lua",
+        requires = {"kyazdani42/nvim-web-devicons"}
+    })
 
-	-- tree explorer
-	use({
-		"kyazdani42/nvim-tree.lua",
-		requires = {
-			"kyazdani42/nvim-web-devicons",
-		},
-	})
+    -- for tabs and stuff
+    use({
+        "akinsho/bufferline.nvim",
+        tag = "v2.*",
+        requires = "kyazdani42/nvim-web-devicons"
+    })
 
-	-- for tabs and stuff
-	use({ "akinsho/bufferline.nvim", tag = "v2.*", requires = "kyazdani42/nvim-web-devicons" })
+    -- visual git integration
+    use({"tanvirtin/vgit.nvim", requires = {"nvim-lua/plenary.nvim"}})
 
-	-- visual git integration
-	use({
-		"tanvirtin/vgit.nvim",
-		requires = {
-			"nvim-lua/plenary.nvim",
-		},
-	})
+    -- more git!
+    use("tpope/vim-fugitive")
+    use("tpope/vim-rhubarb")
 
-	-- more git!
-	use("tpope/vim-fugitive")
-	use("tpope/vim-rhubarb")
+    -- project managementment
+    use("ahmedkhalf/project.nvim")
 
-	-- project managementment
-	use("ahmedkhalf/project.nvim")
+    -- directory specific path, etc
+    use("direnv/direnv.vim")
 
-	-- directory specific path, etc
-	use("direnv/direnv.vim")
+    -- colorschemes
+    use("ayu-theme/ayu-vim")
+    use("averak/laserwave.vim")
+    use("rafamadriz/neon")
+    use("folke/tokyonight.nvim")
 
-	-- colorschemes
-	use("ayu-theme/ayu-vim")
-	use("averak/laserwave.vim")
-	use("rafamadriz/neon")
-	use("folke/tokyonight.nvim")
+    -- quicfix and diagnostic type stuff
+    use({
+        "folke/trouble.nvim",
+        requires = "kyazdani42/nvim-web-devicons",
+        config = function() require("trouble").setup({}) end
+    })
 
-	-- quicfix and diagnostic type stuff
-	use({
-		"folke/trouble.nvim",
-		requires = "kyazdani42/nvim-web-devicons",
-		config = function()
-			require("trouble").setup({})
-		end,
-	})
+    use("gpanders/editorconfig.nvim")
+    use("mg979/vim-visual-multi")
 
-	use("gpanders/editorconfig.nvim")
-	use("mg979/vim-visual-multi")
+    use({"mhinz/vim-grepper", cmd = "Grepper"})
+    use({"junegunn/fzf", run = function() vim.fn["fzf#install"]() end})
 
-	use({ "mhinz/vim-grepper", cmd = "Grepper" })
-	use({
-		"junegunn/fzf",
-		run = function()
-			vim.fn["fzf#install"]()
-		end,
-	})
+    use({"kevinhwang91/nvim-bqf"})
 
-	use({ "kevinhwang91/nvim-bqf" })
+    use("romainl/vim-qf")
 
-	use("romainl/vim-qf")
+    use("sickill/vim-pasta")
+    use("windwp/nvim-autopairs")
+    use("ur4ltz/surround.nvim")
+    use("AndrewRadev/splitjoin.vim")
 
-	use("sickill/vim-pasta")
-	use("windwp/nvim-autopairs")
-	use("ur4ltz/surround.nvim")
-	use("AndrewRadev/splitjoin.vim")
+    -- session, remembering where we were
+    use("farmergreg/vim-lastplace")
+    use("rmagatti/auto-session")
 
-	-- session, remembering where we were
-	use("farmergreg/vim-lastplace")
-	use("rmagatti/auto-session")
+    use("tpope/vim-characterize")
 
-	use("tpope/vim-characterize")
+    use({
+        "nvim-telescope/telescope-frecency.nvim",
+        config = function() end,
+        requires = {"tami5/sqlite.lua"}
+    })
 
-	use({
-		"nvim-telescope/telescope-frecency.nvim",
-		config = function() end,
-		requires = { "tami5/sqlite.lua" },
-	})
+    use("gelguy/wilder.nvim")
+    use("raghur/fruzzy", {cmd = "fruzzy#install()"})
+
+    use({
+        "roxma/nvim-yarp",
+        run = "pip install -r requirements.txt"
+    })
+
+    use('romgrk/fzy-lua-native', {run = "make", on = 'CmdlineEnter'})
 end)
--- lua/ui.lua
+++ lua/ui.lua
@@ -92,6 +92,76 @@ dashboard.section.buttons.val = {
 }
 alpha.setup(dashboard.config)
 
+-- enhance wildmenu
+local wilder = require('wilder')
+wilder.setup({modes = {':'}})
+
+local pipeline = {
+  wilder.branch(
+    wilder.python_file_finder_pipeline({
+      file_command = function(ctx, arg)
+        if string.find(arg, '.') ~= nil then
+            return {'rg', '--files', '--hidden'}
+        else
+            return {'rg', '--files'}
+        end
+      end,
+      dir_command = {'fd', '-td'},
+      filters = {'fruzzy_filter'}
+
+    }),
+    wilder.substitute_pipeline({
+      pipeline = wilder.python_search_pipeline({
+        skip_cmdtype_check = 1,
+        pattern = wilder.python_fuzzy_pattern({
+          start_at_boundary = 0,
+        }),
+      }),
+    }),
+    wilder.cmdline_pipeline({
+      fuzzy = 2,
+      fuzzy_filter = wilder.lua_fzy_filter(),
+    }),
+    {
+      wilder.check(function(_, x) return x == '' end),
+      wilder.history(),
+    },
+    wilder.python_search_pipeline({
+      pattern = wilder.python_fuzzy_pattern({
+        start_at_boundary = 0,
+      }),
+    })
+  ),
+}
+wilder.set_option('pipeline', pipeline)
+
+local popupmenu_renderer = wilder.popupmenu_renderer(
+    wilder.popupmenu_border_theme({
+        border = 'rounded',
+        highlighter = {
+            wilder.lua_fzy_highlighter(),
+            wilder.basic_highlighter(),
+        },
+        highlights = {
+            accent = wilder.make_hl('WilderAccent', 'Pmenu', {{a = 1}, {a = 1}, {foreground = '#f4468f'}}),
+        },
+        left = {
+            ' ',
+            wilder.popupmenu_devicons(),
+            wilder.popupmenu_buffer_flags({
+                flags = ' a + ',
+                icons = {['+'] = '', a = '', h = ''},
+            }),
+        },
+        right = {
+            ' ',
+            wilder.popupmenu_scrollbar(),
+        },
+    })
+)
+
+wilder.set_option('renderer', popupmenu_renderer)
+
 -- mouse
 vim.o.mouse = "a"
 
