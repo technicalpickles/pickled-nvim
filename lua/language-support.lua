10dd43a144fa5efd815dcb53c646efd4fef060a6
Josh Nichols <josh.nichols@gusto.com>
2022-07-16 21:43:26 -0400

    Configure sumneko_lua for nvim

--- lua/language-support.lua
+++ lua/language-support.lua
@@ -31,6 +31,29 @@ lsp.setup_nvim_cmp({
 	},
 })
 
+lsp.configure('sumneko_lua', {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+})
+
 lsp.setup()
 
 require("nvim-treesitter.configs").setup({
