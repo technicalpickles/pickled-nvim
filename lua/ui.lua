54fed0f67a80fd7c03d5454e01764d4783023a94
Josh Nichols <josh.nichols@gusto.com>
2022-07-16 21:46:08 -0400

    disable a large chunk of wilder since python stuff wasn't working on other laptop

--- lua/ui.lua
+++ lua/ui.lua
@@ -17,8 +17,8 @@ vim.cmd([[colorscheme tokyonight]])
 
 -- statusline
 require("lualine").setup({
-	extensions = { "quickfix", "nvim-tree", "toggleterm", "quickfix",  },
-	theme = "tokyonight",
+    extensions = { "quickfix", "nvim-tree", "toggleterm", "quickfix", },
+    theme = "tokyonight",
 })
 
 -- font
@@ -30,18 +30,18 @@ require("gui-font-resize").setup({
 -- tabs
 -- see barbar in plugin.lua
 require("bufferline").setup({
-	options = {
-		close_icon = "✕",
-		buffer_close_icon = "✕",
-		offsets = {
-			{
-				filetype = "NvimTree",
-				text = "",
-				highlight = "Directory",
-				text_align = "left",
-			},
-		},
-	},
+    options = {
+        close_icon = "✕",
+        buffer_close_icon = "✕",
+        offsets = {
+            {
+                filetype = "NvimTree",
+                text = "",
+                highlight = "Directory",
+                text_align = "left",
+            },
+        },
+    },
 })
 -- always show tab line, even if there is only 1 item
 vim.o.showtabline = 2
@@ -60,17 +60,17 @@ vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
 
 -- TODO: can we make the current context bold in the same color?
 require("indent_blankline").setup({
-	show_current_context = true,
-	show_current_context_start = true,
-	char_highlight_list = {
-		"IndentBlanklineIndent1",
-		"IndentBlanklineIndent2",
-		"IndentBlanklineIndent3",
-		"IndentBlanklineIndent4",
-		"IndentBlanklineIndent5",
-		"IndentBlanklineIndent6",
-	},
-	filetype_exclude = { "markdown", "help", "log", "txt" },
+    show_current_context = true,
+    show_current_context_start = true,
+    char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+        "IndentBlanklineIndent3",
+        "IndentBlanklineIndent4",
+        "IndentBlanklineIndent5",
+        "IndentBlanklineIndent6",
+    },
+    filetype_exclude = { "markdown", "help", "log", "txt" },
 })
 
 -- gutter
@@ -80,8 +80,8 @@ vim.o.relativenumber = true
 
 -- cursorline
 require("nvim-cursorline").setup({
-	cursorline = { enable = true, timeout = 1000, number = false },
-	cursorword = { enable = true, min_length = 3, hl = { underline = true } },
+    cursorline = { enable = true, timeout = 1000, number = false },
+    cursorword = { enable = true, min_length = 3, hl = { underline = true } },
 })
 
 vim.o.showmode = false
@@ -90,81 +90,82 @@ vim.o.showmode = false
 local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
 dashboard.section.buttons.val = {
-	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("r", "  Restore session", "<Cmd>RestoreSession<CR>"),
-	dashboard.button("p", "  Open Project", "<Cmd>Telescope projects<CR>"),
-	dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
+    dashboard.button("r", "  Restore session", "<Cmd>RestoreSession<CR>"),
+    dashboard.button("e", "  New file", ":ene<CR>"),
+    dashboard.button("p", "  Open Project", "<Cmd>Telescope projects<CR>"),
+    dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
 }
 alpha.setup(dashboard.config)
 
+-- FIXME: not working on second machine due to python failing to initialize
 -- enhance wildmenu
-local wilder = require("wilder")
-wilder.setup({ modes = { ":" } })
+-- local wilder = require("wilder")
+-- wilder.setup({ modes = { ":" } })
 
-local pipeline = {
-	wilder.branch(
-		wilder.python_file_finder_pipeline({
-			file_command = function(ctx, arg)
-				if string.find(arg, ".") ~= nil then
-					return { "rg", "--files", "--hidden" }
-				else
-					return { "rg", "--files" }
-				end
-			end,
-			dir_command = { "fd", "-td" },
-			filters = { "fruzzy_filter" },
-		}),
-		wilder.substitute_pipeline({
-			pipeline = wilder.python_search_pipeline({
-				skip_cmdtype_check = 1,
-				pattern = wilder.python_fuzzy_pattern({
-					start_at_boundary = 0,
-				}),
-			}),
-		}),
-		wilder.cmdline_pipeline({
-			fuzzy = 2,
-			fuzzy_filter = wilder.lua_fzy_filter(),
-		}),
-		{
-			wilder.check(function(_, x)
-				return x == ""
-			end),
-			wilder.history(),
-		},
-		wilder.python_search_pipeline({
-			pattern = wilder.python_fuzzy_pattern({
-				start_at_boundary = 0,
-			}),
-		})
-	),
-}
-wilder.set_option("pipeline", pipeline)
+-- local pipeline = {
+-- 	wilder.branch(
+-- 		wilder.python_file_finder_pipeline({
+-- 			file_command = function(ctx, arg)
+-- 				if string.find(arg, ".") ~= nil then
+-- 					return { "rg", "--files", "--hidden" }
+-- 				else
+-- 					return { "rg", "--files" }
+-- 				end
+-- 			end,
+-- 			dir_command = { "fd", "-td" },
+-- 			filters = { "fruzzy_filter" },
+-- 		}),
+-- 		wilder.substitute_pipeline({
+-- 			pipeline = wilder.python_search_pipeline({
+-- 				skip_cmdtype_check = 1,
+-- 				pattern = wilder.python_fuzzy_pattern({
+-- 					start_at_boundary = 0,
+-- 				}),
+-- 			}),
+-- 		}),
+-- 		wilder.cmdline_pipeline({
+-- 			fuzzy = 2,
+-- 			fuzzy_filter = wilder.lua_fzy_filter(),
+-- 		}),
+-- 		{
+-- 			wilder.check(function(_, x)
+-- 				return x == ""
+-- 			end),
+-- 			wilder.history(),
+-- 		},
+-- 		wilder.python_search_pipeline({
+-- 			pattern = wilder.python_fuzzy_pattern({
+-- 				start_at_boundary = 0,
+-- 			}),
+-- 		})
+-- 	),
+-- }
+-- wilder.set_option("pipeline", pipeline)
 
-local popupmenu_renderer = wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
-	border = "rounded",
-	highlighter = {
-		wilder.lua_fzy_highlighter(),
-		wilder.basic_highlighter(),
-	},
-	highlights = {
-		accent = wilder.make_hl("WilderAccent", "Pmenu", { { a = 1 }, { a = 1 }, { foreground = "#f4468f" } }),
-	},
-	left = {
-		" ",
-		wilder.popupmenu_devicons(),
-		wilder.popupmenu_buffer_flags({
-			flags = " a + ",
-			icons = { ["+"] = "", a = "", h = "" },
-		}),
-	},
-	right = {
-		" ",
-		wilder.popupmenu_scrollbar(),
-	},
-}))
+-- local popupmenu_renderer = wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+-- 	border = "rounded",
+-- 	highlighter = {
+-- 		wilder.lua_fzy_highlighter(),
+-- 		wilder.basic_highlighter(),
+-- 	},
+-- 	highlights = {
+-- 		accent = wilder.make_hl("WilderAccent", "Pmenu", { { a = 1 }, { a = 1 }, { foreground = "#f4468f" } }),
+-- 	},
+-- 	left = {
+-- 		" ",
+-- 		wilder.popupmenu_devicons(),
+-- 		wilder.popupmenu_buffer_flags({
+-- 			flags = " a + ",
+-- 			icons = { ["+"] = "", a = "", h = "" },
+-- 		}),
+-- 	},
+-- 	right = {
+-- 		" ",
+-- 		wilder.popupmenu_scrollbar(),
+-- 	},
+-- }))
 
-wilder.set_option("renderer", popupmenu_renderer)
+-- wilder.set_option("renderer", popupmenu_renderer)
 
 -- mouse
 vim.o.mouse = "a"
