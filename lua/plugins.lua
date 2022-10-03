e81ba58db4ac8113b98a091c25a9c1981ea00d9a
Josh Nichols <josh.nichols@gusto.com>
2022-10-03 11:25:29 -0400

    Add playground to help with debugging treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -58,6 +58,8 @@ return require("packer").startup(function(use)
 
 	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
 
+	use("nvim-treesitter/playground")
+
 	use("nvim-treesitter/nvim-treesitter-textobjects")
 	use("RRethy/nvim-treesitter-textsubjects")
 	use("nvim-treesitter/nvim-treesitter-refactor")
