2008c8a70e6b09f62645852846effd38a2784ae9
Josh Nichols <josh.nichols@gusto.com>
2022-11-26 22:31:01 -0500

    configure symbols outline

--- lua/navigation.lua
+++ lua/navigation.lua
@@ -52,7 +52,61 @@ require("nvim-tree").setup({
 		enable = true,
 		update_cwd = true,
 	},
+	view = {
+		mappings = {
+			list = {
+				{ key = {"?"}, action = "toggle_help" }
+			}
+		}
+	}
 })
+
+
+require("symbols-outline").setup({
+	-- https://github.com/simrat39/symbols-outline.nvim/issues/185
+	symbols = {
+		File = {hl = "@text.uri" },
+		Module = {hl = "@namespace" },
+		Namespace = {hl = "@namespace" },
+		Package = {hl = "@namespace" },
+		Class = {hl = "@type" },
+		Method = {hl = "@method" },
+		Property = {hl = "@method" },
+		Field = {hl = "@field" },
+		Constructor = {hl = "@constructor" },
+		Enum = {hl = "@type" },
+		Interface = {hl = "@type" },
+		Function = {hl = "@function" },
+		Variable = {hl = "@constant" },
+		Constant = {hl = "@constant" },
+		String = {hl = "@string" },
+		Number = {hl = "@number" },
+		Boolean = {hl = "@boolean" },
+		Array = {hl = "@constant" },
+		Object = {hl = "@type" },
+		Key = {hl = "@type" },
+		Null = {hl = "@type" },
+		EnumMember = {hl = "@field" },
+		Struct = {hl = "@type" },
+		Event = {hl = "@type" },
+		Operator = {hl = "@operator" },
+		TypeParameter = {hl = "@parameter" },
+	},
+
+	keymaps = {
+		show_help = '?'
+	}
+})
+local colors = require("tokyonight.colors").setup()
+
+-- copy NvimTreeRootFolder
+vim.api.nvim_set_hl(1, 'FocusedSymbol', {fg = colors.blue, bold = true})
+
+-- FocusedSymbol
+-- Pmenu
+-- SymbolsOutlineConnector
+-- Comment
+
 
 require("marks").setup({
 	sign_priority = 40
