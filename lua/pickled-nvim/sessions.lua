0cbee3f69463dcbbad93b2a1391847fdbf33fd44
Josh Nichols <josh.nichols@gusto.com>
2022-12-04 10:50:00 -0500

    Disable some session stuff

--- lua/pickled-nvim/sessions.lua
+++ lua/pickled-nvim/sessions.lua
@@ -2,8 +2,18 @@ vim.g.lastplace_ignore = "gitcommit,gitrebase,svn,hgcommit,nvimtree,toggleterm"
 
 require("auto-session").setup({
 	log_level = "warning",
+	auto_restore_enabled = false,
+	auto_save_enabled = false,
+	auto_session_enable_last_session = false,
+
 	-- only keep sessions for this directory
 	auto_session_allowed_dirs = {"~/workspace/*"},
+
+	cwd_change_handling = {
+		post_cwd_changed_hook = function()
+			require("lualine").refresh()
+		end,
+	},
 })
 
 vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
