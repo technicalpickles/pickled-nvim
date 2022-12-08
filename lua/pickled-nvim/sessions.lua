40e0c1a3110a6f8dc2161939013f79f294e1694b
Josh Nichols <josh.nichols@gusto.com>
2022-12-08 18:30:31 -0500

    Add some pre-save commands

--- lua/pickled-nvim/sessions.lua
+++ lua/pickled-nvim/sessions.lua
@@ -1,17 +1,29 @@
-vim.g.lastplace_ignore = "gitcommit,gitrebase,svn,hgcommit,nvimtree,toggleterm"
+vim.g.lastplace_ignore = "gitcommit,gitrebase,svn,hgcommit,nvimtree,toggleterm,Trouble"
 
---require("auto-session").setup({
---	log_level = "info",
+require("auto-session").setup({
+	log_level = "info",
+	bypass_session_save_file_types = {
+		'qf',
+		'NvimTree',
+		'Trouble',
+		'Outline',
+	},
 
---	-- only keep sessions for this directory
---	auto_session_allowed_dirs = { "~/workspace/*" },
+	pre_save_cmds = {
+		'cclose', -- quickfix
+		'NvimTreeClose',
+		'SymbolsOutlineClose',
+		'TroubleClose',
+	},
+	-- only keep sessions for this directory
+	auto_session_allowed_dirs = { "~/workspace/*" },
 
---	-- NOTE: be careful changing this, as a few other plugins interact with directory changes:
---	-- * nvim-tree
---	-- * telescope
---	--
---	-- cwd_change_handling = {
---	-- },
---})
+	-- NOTE: be careful changing this, as a few other plugins interact with directory changes:
+	-- * nvim-tree
+	-- * telescope
+	--
+	-- cwd_change_handling = {
+	-- },
+})
 
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
