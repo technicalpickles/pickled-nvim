4d0afa37845f9cfa89d869d3458d60e3929074fd
Josh Nichols <joshua.nichols@gmail.com>
2023-01-15 20:50:54 -0500

    don't use statuscolumn for now

--- lua/pickled-nvim/folds.lua
+++ lua/pickled-nvim/folds.lua
@@ -65,32 +65,32 @@ _G.ScAppropriateLineNo = function(lnum, relnum)
 	end
 end
 
-vim.o.statuscolumn = '%= '
-  .. '%s' -- sign column FIXME: figure out how to put on the other side without having to do a lot of shifting
-  -- .. '%{%v:lua.ScAppropriateLineNo(v:lnum, v:relnum)%}'
-  .. '%{%' -- evaluate this, and then evaluate what it returns
-    .. '&number ?'
-      .. '(v:relnum ?'
-	    .. 'printf("%"..len(line("$")).."s", v:relnum)' -- when showing relative numbers, make sure to pad so things don't shift as you move the cursor
-      .. ':'
-		.. 'v:lnum'
-      .. ')'
-	.. ':'
-      .. '""'
-	.. ' ' -- space between lines and fold
-  .. '%}'
-  .. '%= '
-  .. '%#FoldColumn#' -- highlight group for fold
-  -- .. '%{v:lua.ScFoldToggleIcon(v:lnum)}'
-  .. '%{' -- expression for showing fold expand/colapse
-    .. 'foldlevel(v:lnum) > foldlevel(v:lnum - 1)' -- any folds?
-      .. '? (foldclosed(v:lnum) == -1' -- currently open?
-        .. '? ""' -- point down
-        .. ': ""' -- point to right
-  	  .. ')'
-  	.. ': " "' -- blank for no fold, or inside fold
-  .. '}'
-  .. '%= ' -- spacing between end of column and start of text
+-- vim.o.statuscolumn = '%= '
+--   .. '%s' -- sign column FIXME: figure out how to put on the other side without having to do a lot of shifting
+--   -- .. '%{%v:lua.ScAppropriateLineNo(v:lnum, v:relnum)%}'
+--   .. '%{%' -- evaluate this, and then evaluate what it returns
+--     .. '&number ?'
+--       .. '(v:relnum ?'
+-- 	    .. 'printf("%"..len(line("$")).."s", v:relnum)' -- when showing relative numbers, make sure to pad so things don't shift as you move the cursor
+--       .. ':'
+-- 		.. 'v:lnum'
+--       .. ')'
+-- 	.. ':'
+--       .. '""'
+-- 	.. ' ' -- space between lines and fold
+--   .. '%}'
+--   .. '%= '
+--   .. '%#FoldColumn#' -- highlight group for fold
+--   -- .. '%{v:lua.ScFoldToggleIcon(v:lnum)}'
+--   .. '%{' -- expression for showing fold expand/colapse
+--     .. 'foldlevel(v:lnum) > foldlevel(v:lnum - 1)' -- any folds?
+--       .. '? (foldclosed(v:lnum) == -1' -- currently open?
+--         .. '? ""' -- point down
+--         .. ': ""' -- point to right
+--   	  .. ')'
+--   	.. ': " "' -- blank for no fold, or inside fold
+--   .. '}'
+--   .. '%= ' -- spacing between end of column and start of text
 
 vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
