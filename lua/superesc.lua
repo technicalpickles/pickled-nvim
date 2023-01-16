a83d0b8d041b82e094a1e30cecaff89c26647479
Josh Nichols <joshua.nichols@gmail.com>
2023-01-15 20:52:08 -0500

    restructure superesc a little. add default mapping

--- lua/superesc.lua
+++ lua/superesc.lua
@@ -1,14 +1,18 @@
 local M = {}
 
-M.close_for_type = {
-	NvimTree = 'NvimTreeClose',
-	Outline = 'SymbolsOutlineClose',
-	Trouble = 'TroubleClose',
-	qf = 'cclose',
+M.default_config = {
+	close_for_type = {
+		NvimTree = 'NvimTreeClose',
+		Outline = 'SymbolsOutlineClose',
+		Trouble = 'TroubleClose',
+		qf = 'cclose',
+	}
 }
 
+M.config =  vim.tbl_extend('keep', M.default_config, {})
+
 M.close_panel = function()
-	local command = M.close_for_type[vim.bo.filetype]
+	local command = M.config.close_for_type[vim.bo.filetype]
 	if command then
 		vim.cmd(command)
 		return true
@@ -22,7 +26,7 @@ M.close_split = function()
 		local bufnr = vim.api.nvim_win_get_buf(winid)
 		local ft = vim.api.nvim_buf_get_option(bufnr, 'filetype')
 
-		if not M.close_for_type[ft] then
+		if not M.config.close_for_type[ft] then
 			table.insert(split_wins, winid)
 		end
 	end
@@ -37,11 +41,18 @@ end
 M.close = function()
 	-- try to close panel first
 	if M.close_panel() then
+		-- but stop so we don't mess with any other windows
 		return
 	end
 
 	M.close_split()
 	-- special window, ie bottom or side pane
+end
+
+M.setup = function(config)
+    M.config = vim.tbl_extend("force", M.config, config or {})
+
+	vim.keymap.set("n", "<Esc>", "<CMD>lua require('superesc').close_panel()<CR>")
 end
 
 return M
