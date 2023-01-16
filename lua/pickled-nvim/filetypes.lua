accbf65d0e91454946e592608de6bee1c05b0115
Josh Nichols <joshua.nichols@gmail.com>
2023-01-15 20:51:07 -0500

    Add more markdown options

--- lua/pickled-nvim/filetypes.lua
+++ lua/pickled-nvim/filetypes.lua
@@ -53,6 +53,15 @@ api.nvim_create_autocmd("FileType", {
 })
 
 g.vim_markdown_edit_url_in = 'current'
+g.vim_markdown_strikethrough = 1
+g.vim_markdown_frontmatter = 1
+g.vim_markdown_fenced_languages = {
+	'c++=cpp',
+	'viml=vim',
+	'bash=sh',
+	'ini=dosini'
+}
+g.vim_markdown_new_list_item_indent = 0
 
 -- ufo / lsp folding is more accurate than vim-markdown
 g.vim_markdown_folding_disabled = 1
