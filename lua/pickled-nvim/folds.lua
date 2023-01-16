2f7f93b4d0663b7cf347808904a7f158e9ef3a99
Josh Nichols <joshua.nichols@gmail.com>
2023-01-15 21:01:44 -0500

    Add back foldcolumn width

--- lua/pickled-nvim/folds.lua
+++ lua/pickled-nvim/folds.lua
@@ -1,5 +1,5 @@
 -- from nvim-ufo
-vim.o.foldcolumn = '0'
+vim.o.foldcolumn = '1'
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
