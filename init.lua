20e3dcf2ec7cde5776146b43e50fd6ccdb598ae6
Josh Nichols <josh.nichols@gusto.com>
2023-01-30 10:55:43 -0500

    delete dead code

--- init.lua
+++ init.lua
@@ -31,15 +31,5 @@ require("pickled-nvim.navigation")
 require("pickled-nvim.pairs")
 require("pickled-nvim.language-support").setup() -- requires folds
 
-require("pickled-nvim.ide")
-require("pickled-nvim.second-brain")
-require("pickled-nvim.chatgpt")
-
 require("pickled-nvim.custom-help")
 require("pickled-nvim.legendary-which-key-palette")
-
--- require("project_nvim").setup({})
-
--- FIXME default node neovide sees is /opt/homebrew/bin/node, which is the latest 18.x ... but there are issues on Apple Silicon
--- see various issues on community ie https://github.com/community/community/discussions/16298
-vim.g.copilot_node_command = "/opt/homebrew/opt/node@16/bin/node"
