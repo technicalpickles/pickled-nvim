2f904b53e946092bd9939bd7cfde48576832bf50
Josh Nichols <josh.nichols@gusto.com>
2023-02-04 16:05:02 -0500

    fiz lazy statuscol

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -1,19 +1,19 @@
 return {
 	{
-		  "luukvbaal/statuscol.nvim",
-		  opts = {
-			  -- order of the fold, sign, line number and separator segments
-			  -- F - fold
-			  -- N - line number
-			  -- S - sign
-			  -- s - separator
-			  order = "NSFs",
-			  relculright = true,
-			  separator = " ",
-			  foldfunc = "builtin",
-			  setopt = true,
-			  ft_ignore = require("pickled-nvim").filetype_config.statuscol.ignore,
-		  },
-		  lazy = false,
+		"luukvbaal/statuscol.nvim",
+		opts = {
+			-- order of the fold, sign, line number and separator segments
+			-- F - fold
+			-- N - line number
+			-- S - sign
+			-- s - separator
+			order = "NSFs",
+			relculright = true,
+			separator = " ",
+			foldfunc = "builtin",
+			setopt = true,
+			ft_ignore = require("pickled-nvim").filetype_config.statuscol.ignore,
+		},
+		event = "VeryLazy",
 	},
 }
