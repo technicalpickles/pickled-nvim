dd7f646d488565d1f0875f3e3e9ce30df96b5601
Josh Nichols <josh.nichols@gusto.com>
2023-03-22 13:42:50 -0400

    don't debug lsp anymore

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -228,7 +228,7 @@ return {
 			)
 
 			-- debug lsp
-			vim.lsp.set_log_level("trace")
+			-- vim.lsp.set_log_level("trace")
 			if vim.fn.has("nvim-0.5.1") == 1 then
 				require("vim.lsp.log").set_format_func(vim.inspect)
 			end
