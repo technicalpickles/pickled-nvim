de83c4c10755f32aa6d741ef41ccb157f3386998
Josh Nichols <josh.nichols@gusto.com>
2023-09-05 14:46:58 -0400

    use hbac fork. update

--- lua/plugins/buffers.lua
+++ lua/plugins/buffers.lua
@@ -2,9 +2,10 @@ local silent_noremap = { noremap = true, silent = true }
 local silent_noremap_both_modes = vim.tbl_deep_extend("keep", silent_noremap, { mode = { "n", "i" } })
 return {
 	{
-		"axkirillov/hbac.nvim",
+		"al-ce/hbac.nvim",
+		branch = "picker_opts",
 		opts = {
-			threshold = 20,
+			-- threshold = 40,
 			telescope = {
 				pin_icons = {
 					unpinned = { "" },
@@ -15,7 +16,21 @@ return {
 		cmd = "Hbac",
 		event = "VeryLazy",
 		keys = {
-			{ "<D-k>", "<cmd>Hbac telescope<CR>", silent_noremap_both_modes },
+			{
+				"<D-k>",
+				function()
+					local default_opts = {
+						show_all_buffers = true,
+						sort_mru = true,
+						sort_lastused = true,
+						ignore_current_buffer = true,
+						cwd_only = false,
+						cwd = nil,
+					}
+					require("hbac").telescope(default_opts)
+				end,
+				silent_noremap_both_modes,
+			},
 		},
 		command_center = {
 			{ desc = "Buffer: Toggle Pin", cmd = "<CMD>Hbac toggle_pin<CR>" },
