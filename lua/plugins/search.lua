75ad502cfc06b2b51bcc1834759159e22368c192
Josh Nichols <josh.nichols@gusto.com>
2023-10-26 10:12:31 -0400

    switch to vim-ripgrep to fix problems with grepper

--- lua/plugins/search.lua
+++ lua/plugins/search.lua
@@ -1,7 +1,21 @@
 local silent_noremap = { noremap = true, silent = true }
 return {
 	{
+		"kyoh86/vim-ripgrep",
+		cmd = "Rg",
+		config = function()
+			vim.cmd([[
+				command! -nargs=+ -complete=file Rg :call ripgrep#search(<q-args>)
+			]])
+		end,
+		keys = {
+			{ "<leader>F", ":Rg ", silent_noremap },
+			{ "<D-F>", ":Rg ", silent_noremap },
+		},
+	},
+	{
 		"mhinz/vim-grepper",
+		enabled = false,
 		cmd = "Grepper",
 		keys = {
 			{ "<leader>F", ":Grepper<CR>", silent_noremap },
