e20519d3d596c6872611b78c5606854d0a8f3ab1
Josh Nichols <josh.nichols@gusto.com>
2023-11-08 10:31:07 -0500

    skip most setup in vscode

--- init.lua
+++ init.lua
@@ -1,24 +1,29 @@
--- barebones config (mostly vim.g and vim.o)
-require("pickled-nvim.basics")
 
--- setup lazy
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
+if vim.g.vscode then
+    -- VSCode extension
+else
+	-- barebones config (mostly vim.g and vim.o)
+	require("pickled-nvim.basics")
+	-- setup lazy
+	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+	if not vim.loop.fs_stat(lazypath) then
+		vim.fn.system({
+			"git",
+			"clone",
+			"--filter=blob:none",
+			"https://github.com/folke/lazy.nvim.git",
+			"--branch=stable", -- latest stable release
+			lazypath,
+		})
+	end
+	vim.opt.rtp:prepend(lazypath)
+	require("lazy").setup("plugins")
+
+	-- post-lazy config
+	require("pickled-nvim").setup()
+	require("pickled-nvim.keybindings")
+	require("superesc").setup({
+		last_window_behavior = "scratch",
 	})
 end
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
 
--- post-lazy config
-require("pickled-nvim").setup()
-require("pickled-nvim.keybindings")
-require("superesc").setup({
-	last_window_behavior = "scratch",
-})
