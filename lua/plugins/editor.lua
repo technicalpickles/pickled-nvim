0b745107e8cea56bce4da5386fa840edb8ba6f37
Josh Nichols <josh.nichols@gusto.com>
2024-03-21 08:36:15 -0400

    fix mappings

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -180,46 +180,25 @@ return {
 		keys = {
 			{
 				"n",
-				"n",
 				[[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
 				kopts,
 			},
 			{
-				"n",
 				"N",
 				[[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
 				kopts,
 			},
-			{ "n", "*", [[*<Cmd>lua require('hlslens').start()<CR>]], kopts },
-			{ "n", "#", [[#<Cmd>lua require('hlslens').start()<CR>]], kopts },
-			{ "n", "g*", [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts },
-			{ "n", "g#", [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts },
-			{ "n", "<Leader>l", "<Cmd>noh<CR>", kopts },
+			{ "*", [[*<Cmd>lua require('hlslens').start()<CR>]], kopts },
+			{ "#", [[#<Cmd>lua require('hlslens').start()<CR>]], kopts },
+			{ "g*", [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts },
+			{ "g#", [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts },
+			{ "<Leader>l", "<Cmd>noh<CR>", kopts },
 		},
 
 		config = function()
-			require("hlslens").setup()
-
-			local kopts = { noremap = true, silent = true }
-
-			vim.api.nvim_set_keymap(
-				"n",
-				"n",
-				[[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
-				kopts
-			)
-			vim.api.nvim_set_keymap(
-				"n",
-				"N",
-				[[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
-				kopts
-			)
-			vim.api.nvim_set_keymap("n", "*", [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
-			vim.api.nvim_set_keymap("n", "#", [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
-			vim.api.nvim_set_keymap("n", "g*", [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
-			vim.api.nvim_set_keymap("n", "g#", [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
-
-			vim.api.nvim_set_keymap("n", "<Leader>l", "<Cmd>noh<CR>", kopts)
+			require("hlslens").setup({
+				-- TODO customize override_lens to be a little more subtle
+			})
 		end,
 	},
 }
